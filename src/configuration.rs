643caee1ab4ffd77864e0ae59c16a4cf55958b4b
CarterSteele <carter@maetech.ca>
2024-11-02 19:23:56 -0400

    updating git workflow per 3.8.5.5

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,7 @@
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
+    pub application_port: u16,
 }
 
 #[derive(serde::Deserialize)]
@@ -15,9 +15,19 @@ pub struct DatabaseSettings {
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let settings = config::Config::builder()
-        .add_source(
-            config::File::new("configuration.yml", config::FileFormat::Yaml)
-        )
+        .add_source(config::File::new(
+            "configuration.yml",
+            config::FileFormat::Yaml,
+        ))
         .build()?;
     settings.try_deserialize::<Settings>()
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use std::net::TcpListener;
-use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
-
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let config = get_configuration().expect("Failed to read config file");
-    let address = format!("127.0.0.1:{}", {config.application_port});
-    let listener = TcpListener::bind(address)
-        .expect("Failed to bind random port");
+    let address = format!("127.0.0.1:{}", { config.application_port });
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
     run(listener)?.await
 }
