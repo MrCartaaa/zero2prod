7e1bdd63f05962e3aa7a21666220120133d84733
CarterSteele <carter@maetech.ca>
2024-11-02 17:22:41 -0400

    adding database configuration settings, Ch. 3.8.5.3

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")
+    let config = get_configuration().expect("Failed to read config file");
+    let address = format!("127.0.0.1:{}", {config.application_port});
+    let listener = TcpListener::bind(address)
         .expect("Failed to bind random port");
     run(listener)?.await
 }
