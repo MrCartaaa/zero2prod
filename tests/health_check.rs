b0d442bfe093f1abb1e1df5f7d38d2a13967be5f
CarterSteele <carter@maetech.ca>
2024-11-02 16:58:44 -0400

    refactoring code, Ch. 3.8.5.2.1

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,7 +4,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address.");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
@@ -62,6 +62,8 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             .await
             .expect("Failed to execute request.");
 
-        assert_eq!(400, response.status().as_u16(), "The API did not fail with 400 Bad Request when the payload was {}.", error_message);
+        assert_eq!(400, response.status().as_u16(),
+                   "The API did not fail with 400 Bad Request when the payload was {}.",
+                   error_message);
     }
 }
