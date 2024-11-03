48fcd5477253c71ec9e2f8aa3384241ac2f10b35
CarterSteele <carter@maetech.ca>
2024-11-03 12:38:08 -0500

    dockerizing the app, but the logger isn't working as expected.

--- src/telementry.rs
+++ src/telementry.rs
@@ -25,5 +25,5 @@ where
 
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     LogTracer::init().expect("Failed to set logger");
-    set_global_default(Dispatch::from(subscriber)).expect("Failed to set subscriber");
+    set_global_default(Dispatch::new(subscriber)).expect("Failed to set subscriber");
 }
