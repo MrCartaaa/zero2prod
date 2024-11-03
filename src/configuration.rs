cbc3277d9c99861bdd61b92f6bd7e631851ae3e2
CarterSteele <carter@maetech.ca>
2024-11-02 22:44:08 -0400

    cargo fmt

--- src/configuration.rs
+++ src/configuration.rs
@@ -32,6 +32,9 @@ impl DatabaseSettings {
     }
 
     pub fn connection_string_without_db(&self) -> String {
-        format!("postgres://{}:{}@{}:{}", self.username, self.password, self.host, self.port)
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
     }
 }
