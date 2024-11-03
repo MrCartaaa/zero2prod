f7c4568d30f5405bdd55c7565272ec68223e11ca
CarterSteele <carter@maetech.ca>
2024-11-02 20:18:33 -0400

    finishing up 3.9.2 -> adding PGConnection to main & startup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::net::TcpListener;
+use sqlx::{Connection, PgConnection};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let config = get_configuration().expect("Failed to read config file");
+
+    let connection = PgConnection::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
     let address = format!("127.0.0.1:{}", { config.application_port });
+
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener)?.await
+
+    run(listener, connection)?.await
 }
