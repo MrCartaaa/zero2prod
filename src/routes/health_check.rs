6ea2d2cf5195322a1d410d2ce992ac455e48c2e3
CarterSteele <carter@maetech.ca>
2024-11-03 15:33:17 -0500

    finishing up ch 5, bug fix

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -5,25 +5,3 @@ use sqlx::{Connection, Executor, PgConnection, PgPool};
 pub async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
 }
-
-pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect_with(&config.connect_options())
-        .await
-        .expect("Failed to connect to Postgres");
-
-    connection
-        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
-        .await
-        .expect("Failed to create database");
-
-    let connection_pool = PgPool::connect_with(config.connect_options())
-        .await
-        .expect("Failed to connect to Postgres");
-
-    sqlx::migrate!("./migrations")
-        .run(&connection_pool)
-        .await
-        .expect("Failed to migrate the database");
-
-    connection_pool
-}
