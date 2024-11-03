624a6f5e7d2fb4b38048bcdb1e176f2f3284abe0
CarterSteele <carter@maetech.ca>
2024-11-03 15:52:22 -0500

    finishing up ch 5, lint fix

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,6 +1,4 @@
-use crate::configuration::DatabaseSettings;
 use actix_web::{HttpRequest, HttpResponse, Responder};
-use sqlx::{Connection, Executor, PgConnection, PgPool};
 
 pub async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
