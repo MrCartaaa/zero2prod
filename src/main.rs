4955fc6beb3baee176f04aa9601f04c7fc39a7ff
CarterSteele <carter@maetech.ca>
2024-11-02 21:16:18 -0400

    finishing up Ch. 3 -> adding PgPool and the related changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
-use sqlx::{Connection, PgConnection};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
@@ -7,13 +7,13 @@ use zero2prod::startup::run;
 async fn main() -> Result<(), std::io::Error> {
     let config = get_configuration().expect("Failed to read config file");
 
-    let connection = PgConnection::connect(&config.database.connection_string())
+    let connection_pool = PgPool::connect(&config.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
 
     let address = format!("127.0.0.1:{}", { config.application_port });
 
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+    let listener = TcpListener::bind(address)?;
 
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
