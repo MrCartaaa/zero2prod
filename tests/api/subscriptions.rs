ca6c8e7a26ce5f5f7547840ae8aeb7044811fb95
CarterSteele <carter@maetech.ca>
2024-11-22 12:31:56 -0500

    All of Ch. 8 -> Error Handling.

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -120,17 +120,18 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
 
-#[tokio::test]
-async fn subscribe_fails_if_there_is_a_fatal_database_error() {
-    let app = spawn_app().await;
-    let body = "name=leguin&email=ursula_le_guin%40gmail.com";
-
-    sqlx::query("ALTER TABLE subscriptions DROP column email;")
-        .execute(&app.db_pool)
-        .await
-        .unwrap();
-
-    let resp = app.post_subscription(body.into()).await;
-
-    assert_eq!(resp.status().as_u16(), 200)
-}
+// this is commented out, use it to test the Error Handling, Logging of the post_subscription() fn
+// #[tokio::test]
+// async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+//     let app = spawn_app().await;
+//     let body = "name=leguin&email=ursula_le_guin%40gmail.com";
+//
+//     sqlx::query("ALTER TABLE subscriptions DROP column email;")
+//         .execute(&app.db_pool)
+//         .await
+//         .unwrap();
+//
+//     let resp = app.post_subscription(body.into()).await;
+//
+//     assert_eq!(resp.status().as_u16(), 200)
+// }
